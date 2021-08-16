6ca49d27a345cc4a3a789080b10b306531670a03
naldal <cec7378@gmail.com>
2021-08-16 16:52:27 +0900

    Initial Commit

---
+++ QuizSwift/QuizSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizSwiftApp.swift
+//  QuizSwift
+//
+//  Created by 송하민 on 2021/08/16.
+//
+
+import SwiftUI
+
+@main
+struct QuizSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
